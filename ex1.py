68ad51c31beaafff225416d4ee61bcdc6593cdeb
Romain Grondin <romain.grondin@epitech.eu>
2021-06-11 16:21:30 +0400

    added detect_hand

--- ex1.py
+++ ex1.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import cv2
 import sys
+import mediapipe as mp
 
 def open_image():
     image = cv2.imread(sys.argv[1])
@@ -23,4 +24,21 @@ def open_video():
             break
     cv2.destroyAllWindows()
 
-open_video()
+def detect_hand():
+    cam = cv2.VideoCapture(0)
+    mpHand = mp.solutions.hands
+    hands = mpHand.Hands()
+    while True:
+        ret, frame = cam.read()
+        if not ret:
+            print("unable to open camera")
+            break
+        rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        results = hands.process(rgb)
+        print(results.multi_hand_landmarks)
+        cv2.imshow("Camera", frame)
+        if cv2.waitKey(1) == ord('q'):
+            break
+    cv2.destroyAllWindows()
+
+detect_hand()

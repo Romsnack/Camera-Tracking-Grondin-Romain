1f4fb8cacc48836612e7d73bf1a4fdd0f2abeff7
Romain Grondin <romain.grondin@epitech.eu>
2021-06-11 16:35:44 +0400

    added draw_plan to detect hands on screen

--- ex1.py
+++ ex1.py
@@ -41,4 +41,27 @@ def detect_hand():
             break
     cv2.destroyAllWindows()
 
-detect_hand()
+def draw_plan():
+    cam = cv2.VideoCapture(0)
+    mpHand = mp.solutions.hands
+    hands = mpHand.Hands()
+    mpDraw = mp.solutions.drawing_utils
+    while True:
+        ret, frame = cam.read()
+        if not ret:
+            print("unable to open camera")
+            break
+        rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        results = hands.process(rgb)
+        # print(results.multi_hand_landmarks)
+        if results.multi_hand_landmarks:
+            for handlms in results.multi_hand_landmarks:
+                mpDraw.draw_landmarks(frame, handlms)
+                cv2.imshow("Camera", frame)
+                if cv2.waitKey(1) == ord('q'):
+                    break
+        if cv2.waitKey(1) == ord('q'):
+            break
+    cv2.destroyAllWindows()
+
+draw_plan()
